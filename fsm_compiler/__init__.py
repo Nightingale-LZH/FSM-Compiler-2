2150941baebe9d99b2506ee909836eb3a67dccef
Zihui Liu <44153062+Nightingale-LZH@users.noreply.github.com>
2024-08-30 16:18:40 +0800

    Add support to mermaid and graphvis visualization support

--- fsm_compiler/__init__.py
+++ fsm_compiler/__init__.py
@@ -1,4 +1,3 @@
-from .parser import CHARACTERS
-from .parser import parse_to_AST, generate_AST_from_code
+from .parser import CHARACTERS, parse_to_AST, generate_AST_from_code
 from .assembler import generate_FSM_from_AST, optimize_FSM
-from .code_gen import generate_code_from_FSM
+from .code_gen import generate_code_from_FSM, generate_graphviz_dot_visualization_from_FSM, generate_mermaid_visualization_from_FSM
--- tests/test_code_gen.py
+++ tests/test_code_gen.py
@@ -12,7 +12,7 @@ import fsm_compiler.assembler as assembler
 import fsm_compiler.code_gen as code_gen
 from fsm_compiler.ast_types import *
 
-class TestParserPrettyPrint(unittest.TestCase):
+class TestCodeGenCPP(unittest.TestCase):
     def test_code_gen_1(self):
         s = """
         FSM function_name_opt9() { 
@@ -51,6 +51,7 @@ class TestParserPrettyPrint(unittest.TestCase):
         fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
         # print(code_gen.fsm_to_graphviz_dot(fsm.starting_node))
         # print(code_gen.generate_code_from_FSM(fsm))
+        self.assertEqual(len(code_gen.generate_code_from_FSM(fsm)), 3846)
         
     def test_code_gen_2(self):
         s = """
@@ -69,6 +70,94 @@ class TestParserPrettyPrint(unittest.TestCase):
         fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
         # print(code_gen.fsm_to_graphviz_dot(fsm.starting_node))
         # print(code_gen.generate_code_from_FSM(fsm))
+        self.assertEqual(len(code_gen.generate_code_from_FSM(fsm)), 1968)
+        
+        
+class TestCodeGenMermaid(unittest.TestCase):
+    def test_code_gen_1(self):
+        s = """
+        FSM function_name_opt9() { 
+            GLOBAL int a = 0;
+            DO {
+                IF (a == 0) {
+                    b++;
+                    WAIT(100);
+                } ELSE IF (b == 0) {
+                    b--;
+                    WAIT(200);
+                }
+            } WHILE(a != 0);
+        }
+        """
+        fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
+        # print(code_gen.generate_code_from_FSM(fsm))
+        # print(code_gen.fsm_to_mermaid(fsm.starting_node))
+        self.assertEqual(len(code_gen.generate_mermaid_visualization_from_FSM(fsm)), 766)
+        
+    def test_code_gen_2(self):
+        s = """
+        FSM function_name_opt9() { 
+            GLOBAL int a = 0;
+            DO {
+                IF (a == 0) {
+                    GLOBAL int b = 0;
+                    b++;
+                    WAIT(100);
+                } ELSE IF (b == 0) {
+                    GLOBAL int another = 0;
+                    b--;
+                    WAIT(200);
+                }
+            } WHILE(a != 0);
+        }
+        """
+        fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
+        # print(code_gen.generate_code_from_FSM(fsm))
+        # print(code_gen.generate_mermaid_visualization_from_FSM(fsm))
+        self.assertEqual(len(code_gen.generate_mermaid_visualization_from_FSM(fsm)), 822)
+        
+class TestCodeGenGraphvizDot(unittest.TestCase):
+    def test_code_gen_1(self):
+        s = """
+        FSM function_name_opt9() { 
+            GLOBAL int a = 0;
+            DO {
+                IF (a == 0) {
+                    b++;
+                    WAIT(100);
+                } ELSE IF (b == 0) {
+                    b--;
+                    WAIT(200);
+                }
+            } WHILE(a != 0);
+        }
+        """
+        fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
+        # print(code_gen.generate_code_from_FSM(fsm))
+        # print(code_gen.generate_graphviz_dot_visualization_from_FSM(fsm))
+        self.assertEqual(len(code_gen.generate_graphviz_dot_visualization_from_FSM(fsm)), 959)
+        
+    def test_code_gen_2(self):
+        s = """
+        FSM function_name_opt9() { 
+            GLOBAL int a = 0;
+            DO {
+                IF (a == 0) {
+                    GLOBAL int b = 0;
+                    b++;
+                    WAIT(100);
+                } ELSE IF (b == 0) {
+                    GLOBAL int another = 0;
+                    b--;
+                    WAIT(200);
+                }
+            } WHILE(a != 0);
+        }
+        """
+        fsm = assembler.generate_FSM_from_AST(parser.generate_AST_from_code(s))
+        # print(code_gen.generate_code_from_FSM(fsm))
+        print(code_gen.generate_graphviz_dot_visualization_from_FSM(fsm))
+        self.assertEqual(len(code_gen.generate_graphviz_dot_visualization_from_FSM(fsm)), 1003)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.CRITICAL)
